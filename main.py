98913d312daac76cd254803404aba0c4300c1dec
Lyre <102676211+Shika-QHQN@users.noreply.github.com>
2023-09-05 15:16:23 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,84 @@
+
+def intro():
+    txt1 = 'Welcome to'
+    new_str = txt1.center(20, '*')                                            
+    a = ''' 
+             )       )                                    
+            /      _/                                     
+           /    _/~O.     ,   O  ____     ,____           
+          /  _/~ /' |    /  /' /'    )   /'    )          
+         /_/~  /'   |  /' /' /(___,/'  /'    /'           
+        /~    (__  _|/(__(__(________/'    /(__           '''
+                                              
+    print(new_str)
+    print(a)
+    print(' \n')
+    print('This is a text editor ')
+    print('Type "end now~~" to exit \nUse "display mode" to review your text\nType "save this **** file" to store it\nIf you want to read file, please type"read this **** file" and then "display mode"')
+
+import os
+
+def save_list_to_file(lst, filename, path):
+    file_path = os.path.join(path, filename)
+    try:
+        with open(file_path, 'w') as file:
+            file.write('\n'.join(lst))
+        print(f"The list has been saved to {file_path} successfully.")
+    except Exception as e:
+        print(f"An error occurred while saving the file: {e}")
+    
+intro()
+#  .|'''|                    .|'''|,   ||    '||            ''|, .''',  ''|, ,'''|, 
+#  ||                        ||   ||   ||     ||           '  || |   | '  ||     || 
+#  `|'''|, .|''|, '||''|,    `|...|| ''||''   ||''|,         .|' |   |   .|'  '''|| 
+#   .   || ||..||  ||  ||         ''   ||     ||  ||        //   |   |  //       || 
+#   |...|' `|...   ||..|'        ''    `|..' .||  || ,,    ((... `,,,' ((... '...|' 
+#                  ||           ''                    ,                             
+#                 .||                                                               
+i = input('~:')
+line = 0
+textdata = []
+
+def read_file(file_path):
+    textdata = []
+    try:
+        file_path = os.path.abspath(file_path)  # Convert to absolute path
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            lines = [line.strip() for line in lines]
+            return lines
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
+    return []
+
+while i != 'end now~~':
+    if i == 'save this **** file':
+        save_path = input('We do you want to store?:')
+        save_name = input('Type an awesome name:')
+        save_list_to_file(textdata, save_name, save_path) 
+        break
+    
+    if i == 'read this **** file':
+        read_path = input('Name the path:')
+        lst_from_file = read_file(read_path)
+        textdata.extend(lst_from_file)
+    
+    space_counter = 0
+    for chch in i:
+        if chch == ' ':
+            space_counter += 1
+        if space_counter >= 2:
+            rows = i.strip().split('\n')
+            for row in rows:
+                textdata.append(row.strip())
+           
+    if i == 'display mode' and line > 0:
+        for lines in range(line):
+            print(lines + 1 ,' ',textdata[lines])
+    else:
+        textdata.append(i)
+    i = input('~:')
+    
+    line += 1
